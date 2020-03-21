5a796c1f831cde7a9564a5200c53933629ce288f
Javier <jortiz.dev@gmail.com>
2020-03-21 14:07:44 +0100

    Add validation to letter

--- app/models/letter.rb
+++ app/models/letter.rb
@@ -1,2 +1,8 @@
 class Letter < ApplicationRecord
+  attr_accessor :title, :author_name, :author_city, :body
+
+  validates :title,       presence: true, length: { maximum: 120 }
+  validates :author_name, presence: true, length: { maximum: 100 }
+  validates :author_city, presence: true, length: { maximum: 120 }
+  validates :body,        presence: true, length: { maximum: 1200 }
 end
