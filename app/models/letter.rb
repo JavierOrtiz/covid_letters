7efbe0dc277b35d85fb27b974945cdac93fb49f8
Javier <jortiz.dev@gmail.com>
2020-03-22 11:34:33 +0100

    Remove lenght for body validation

--- app/models/letter.rb
+++ app/models/letter.rb
@@ -19,5 +19,5 @@ class Letter < ApplicationRecord
   validates :title,       presence: true, length: { maximum: 120 }
   validates :author_name, presence: true, length: { maximum: 100 }
   validates :author_city, presence: true, length: { maximum: 120 }
-  validates :body,        presence: true, length: { maximum: 1200 }
+  validates :body,        presence: true
 end
