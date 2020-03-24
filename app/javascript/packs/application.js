5fc4a77a445b55fa6c48a1dc0e22e7139d2c6937
Javier <jortiz.dev@gmail.com>
2020-03-24 13:41:34 +0100

    Update form and index view with new styles

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,4 +3,6 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
-import '../stylesheets/application'
+import '../stylesheets/application';
+window.jQuery = $;
+window.$ = $;
